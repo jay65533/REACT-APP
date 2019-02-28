04e73425abf881fc9e466df8b3393ae5b8d09148
Jay Nagarsheth <jnagarsheth@yahoo.com>
2019-02-27 21:35:04 -0800

    Update 1
    Finished learning about state

--- complete-react-guide/src/App.js
+++ complete-react-guide/src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Bob"},
+      {name: "Jay"},
+      {name: "Stephanie"}
+
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("was clicked!")
+    this.setState({
+      persons: [
+        {name: "Tabitha"},
+        {name: "Drew"},
+        {name: "Soma"}
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi,Boiiiiiiiiiiiiiiiiiiiiiiiii</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name = {this.state.persons[0].name}>My Hobbies: Gaming</Person>
+        <Person name = {this.state.persons[1].name}>My Hobbies: Racing</Person>
+        <Person name = {this.state.persons[2].name}>My Hobbies: Studying</Person>
       </div>
     );
   }
