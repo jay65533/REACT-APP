3150c5bd2844c3dab157f6d1223acc7298a1bf77
Jay Nagarsheth <jnagarsheth@yahoo.com>
2019-03-08 12:31:07 -0800

    Test commit

--- complete-react-guide/src/App.js
+++ complete-react-guide/src/App.js
@@ -1,73 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+
+function getTime() {
+  return (new Date()).toLocaleTimeString()
+}
 
 class App extends Component {
-  state = {
-    persons: [
-      {name: "Bob"},
-      {name: "Jay"},
-      {name: "Stephanie"}
-    ],
-    showPersons: false
-  }
-
-  switchNameHandler = () => {
-    console.log("was clicked!")
-    this.setState({
-      persons: [
-        {name: "Tabitha"},
-        {name: "Drew"},
-        {name: "Soma"}
-      ]
-    })
-  }
   
-  togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons
-    this.setState({showPersons: !doesShow})
-  }
-
   render() {
-
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '5px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
-
-    let persons = null;
-
-    if(this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map()}
-          <Person 
-            name = {this.state.persons[0].name}
-            click = {this.switchNameHandler}>My Hobbies: Gaming
-          </Person>
-          <Person 
-            name = {this.state.persons[1].name}
-            click = {this.switchNameHandler}>My Hobbies: Racing
-          </Person>
-          <Person 
-            name = {this.state.persons[2].name}
-            click = {this.switchNameHandler}>My Hobbies: Studying
-          </Person>
-        </div>
-      )
-    }
+    const buttonText = "Click Me!"
     return (
-      <div className="App">
-        <h1>Hi,Boiiiiiiiiiiiiiiiiiiiiiiiii</h1>
-        <button 
-          style = {style}
-          onClick={this.togglePersonsHandler}>Toggle Persons
+      <div>
+        <label className = "label" htmlFor="name">
+          Enter name:
+        </label>
+        <input id = "name" type = "text"/>
+        <button style = {{backgroundColor: 'blue', color: 'white'}}>
+          {buttonText} 
         </button>
-        {persons}
+        <div>
+          <h1>Current Time:</h1>
+          <h2>{getTime()}</h2>
+        </div>
       </div>
+      
+      
     );
   }
 }
